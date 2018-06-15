1178e4e999a23ab59c8fa3ead78f81118a25a1c7
kennygonzales <k3n.n.y@hotmail.com>
2018-06-15 20:33:46 +0000

    Servicio de horas disponibles

--- BASE_DE_DATOS_TSYS.sql
+++ BASE_DE_DATOS_TSYS.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 10-06-2018 a las 21:19:18
+-- Tiempo de generación: 15-06-2018 a las 19:50:10
 -- Versión del servidor: 5.5.24-log
 -- Versión de PHP: 5.4.3
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de datos: `t_sys`
+-- Base de datos: `tsys3`
 --
 CREATE DATABASE `t_sys` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `t_sys`;
@@ -29,14 +29,14 @@ USE `t_sys`;
 --
 
 CREATE TABLE IF NOT EXISTS `tb_available_knowledge` (
-  `co_avai_know` int(11) NOT NULL,
+  `co_avai_know` int(11) NOT NULL AUTO_INCREMENT,
   `tb_user_teacher_co_dni_teacher` varchar(8) NOT NULL,
   `tb_career_co_career` int(11) NOT NULL,
-  `no_theme` varchar(256) DEFAULT NULL,
+  `no_theme` varchar(256) NOT NULL,
   PRIMARY KEY (`co_avai_know`),
   KEY `tb_available_knowledge_tb_career_FK` (`tb_career_co_career`),
   KEY `tb_available_knowledge_tb_user_teacher_FK` (`tb_user_teacher_co_dni_teacher`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
@@ -58,8 +58,7 @@ CREATE TABLE IF NOT EXISTS `tb_career` (
 --
 
 CREATE TABLE IF NOT EXISTS `tb_contract` (
-  `co_contract` int(11) NOT NULL,
-  `tb_hours_available_co_dni_teacher` varchar(8) NOT NULL,
+  `co_contract` int(11) NOT NULL AUTO_INCREMENT,
   `tb_hours_available_co_hours_ava` int(11) NOT NULL,
   `tb_available_knowledge_co_avai_know` int(11) NOT NULL,
   `co_grade` int(11) DEFAULT NULL,
@@ -68,13 +67,14 @@ CREATE TABLE IF NOT EXISTS `tb_contract` (
   `date_advisory` date DEFAULT NULL,
   `estate_contract` int(11) DEFAULT NULL,
   `method_payment` int(11) DEFAULT NULL,
-  `nu_latitude` bigint(20) DEFAULT NULL,
-  `nu_longitude` bigint(20) DEFAULT NULL,
+  `nu_latitude` double DEFAULT NULL,
+  `nu_longitude` double DEFAULT NULL,
   `score_contract` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`tb_available_knowledge_co_avai_know`,`tb_hours_available_co_dni_teacher`,`tb_hours_available_co_hours_ava`,`co_contract`),
-  KEY `tb_contract_tb_hours_available_FK` (`tb_hours_available_co_dni_teacher`,`tb_hours_available_co_hours_ava`),
+  PRIMARY KEY (`co_contract`),
+  KEY `tb_contract_tb_available_knowledge_FK` (`tb_available_knowledge_co_avai_know`),
+  KEY `tb_contract_tb_hours_available_FK` (`tb_hours_available_co_hours_ava`),
   KEY `tb_contract_tb_user_student_FK` (`tb_user_student_co_dni_student`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -95,13 +95,14 @@ CREATE TABLE IF NOT EXISTS `tb_grade` (
 --
 
 CREATE TABLE IF NOT EXISTS `tb_hours_available` (
-  `co_hours_ava` int(11) NOT NULL,
+  `co_hours_ava` int(11) NOT NULL AUTO_INCREMENT,
   `tb_user_teacher_co_dni_teacher` varchar(8) NOT NULL,
   `co_day` int(11) DEFAULT NULL,
   `co_hour` int(11) DEFAULT NULL,
   `status_hours_ava` int(11) DEFAULT NULL,
-  PRIMARY KEY (`tb_user_teacher_co_dni_teacher`,`co_hours_ava`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`co_hours_ava`),
+  KEY `tb_hours_available_tb_user_teacher_FK` (`tb_user_teacher_co_dni_teacher`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -110,14 +111,16 @@ CREATE TABLE IF NOT EXISTS `tb_hours_available` (
 --
 
 CREATE TABLE IF NOT EXISTS `tb_teacher_detail_uni` (
+  `co_detail_uni` int(11) NOT NULL AUTO_INCREMENT,
   `tb_user_teacher_co_dni_teacher` varchar(8) NOT NULL,
   `tb_university_co_university` int(11) NOT NULL,
   `tb_grade_co_grade` int(11) DEFAULT NULL,
   `year_egress` int(11) DEFAULT NULL,
-  PRIMARY KEY (`tb_university_co_university`,`tb_user_teacher_co_dni_teacher`),
+  PRIMARY KEY (`co_detail_uni`),
   KEY `tb_teacher_detail_uni_tb_grade_FK` (`tb_grade_co_grade`),
+  KEY `tb_teacher_detail_uni_tb_university_FK` (`tb_university_co_university`),
   KEY `tb_teacher_detail_uni_tb_user_teacher_FK` (`tb_user_teacher_co_dni_teacher`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -166,10 +169,10 @@ CREATE TABLE IF NOT EXISTS `tb_user_teacher` (
   `des_user_phone` varchar(16) DEFAULT NULL,
   `co_user_card` varchar(32) DEFAULT NULL,
   `num_userd_credit_card` varchar(32) DEFAULT NULL,
-  `num_user_nu_latitude` bigint(20) DEFAULT NULL,
-  `num_user_nu_longitude` bigint(20) DEFAULT NULL,
+  `num_user_nu_latitude` double DEFAULT NULL,
+  `num_user_nu_longitude` double DEFAULT NULL,
   `status_user` int(11) DEFAULT NULL,
-  `prom_score` bigint(20) DEFAULT NULL,
+  `prom_score` double DEFAULT NULL,
   PRIMARY KEY (`co_dni_teacher`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -181,16 +184,16 @@ CREATE TABLE IF NOT EXISTS `tb_user_teacher` (
 -- Filtros para la tabla `tb_available_knowledge`
 --
 ALTER TABLE `tb_available_knowledge`
-  ADD CONSTRAINT `tb_available_knowledge_tb_user_teacher_FK` FOREIGN KEY (`tb_user_teacher_co_dni_teacher`) REFERENCES `tb_user_teacher` (`co_dni_teacher`),
-  ADD CONSTRAINT `tb_available_knowledge_tb_career_FK` FOREIGN KEY (`tb_career_co_career`) REFERENCES `tb_career` (`co_career`);
+  ADD CONSTRAINT `tb_available_knowledge_tb_career_FK` FOREIGN KEY (`tb_career_co_career`) REFERENCES `tb_career` (`co_career`),
+  ADD CONSTRAINT `tb_available_knowledge_tb_user_teacher_FK` FOREIGN KEY (`tb_user_teacher_co_dni_teacher`) REFERENCES `tb_user_teacher` (`co_dni_teacher`);
 
 --
 -- Filtros para la tabla `tb_contract`
 --
 ALTER TABLE `tb_contract`
-  ADD CONSTRAINT `tb_contract_tb_user_student_FK` FOREIGN KEY (`tb_user_student_co_dni_student`) REFERENCES `tb_user_student` (`co_dni_student`),
   ADD CONSTRAINT `tb_contract_tb_available_knowledge_FK` FOREIGN KEY (`tb_available_knowledge_co_avai_know`) REFERENCES `tb_available_knowledge` (`co_avai_know`),
-  ADD CONSTRAINT `tb_contract_tb_hours_available_FK` FOREIGN KEY (`tb_hours_available_co_dni_teacher`, `tb_hours_available_co_hours_ava`) REFERENCES `tb_hours_available` (`tb_user_teacher_co_dni_teacher`, `co_hours_ava`);
+  ADD CONSTRAINT `tb_contract_tb_hours_available_FK` FOREIGN KEY (`tb_hours_available_co_hours_ava`) REFERENCES `tb_hours_available` (`co_hours_ava`),
+  ADD CONSTRAINT `tb_contract_tb_user_student_FK` FOREIGN KEY (`tb_user_student_co_dni_student`) REFERENCES `tb_user_student` (`co_dni_student`);
 
 --
 -- Filtros para la tabla `tb_hours_available`
@@ -202,10 +205,11 @@ ALTER TABLE `tb_hours_available`
 -- Filtros para la tabla `tb_teacher_detail_uni`
 --
 ALTER TABLE `tb_teacher_detail_uni`
-  ADD CONSTRAINT `tb_teacher_detail_uni_tb_user_teacher_FK` FOREIGN KEY (`tb_user_teacher_co_dni_teacher`) REFERENCES `tb_user_teacher` (`co_dni_teacher`),
   ADD CONSTRAINT `tb_teacher_detail_uni_tb_grade_FK` FOREIGN KEY (`tb_grade_co_grade`) REFERENCES `tb_grade` (`co_grade`),
-  ADD CONSTRAINT `tb_teacher_detail_uni_tb_university_FK` FOREIGN KEY (`tb_university_co_university`) REFERENCES `tb_university` (`co_university`);
+  ADD CONSTRAINT `tb_teacher_detail_uni_tb_university_FK` FOREIGN KEY (`tb_university_co_university`) REFERENCES `tb_university` (`co_university`),
+  ADD CONSTRAINT `tb_teacher_detail_uni_tb_user_teacher_FK` FOREIGN KEY (`tb_user_teacher_co_dni_teacher`) REFERENCES `tb_user_teacher` (`co_dni_teacher`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
--- core/autoload.php
+++ core/autoload.php
@@ -5,6 +5,7 @@ include_once "student.class.php";
 include_once "teacher.class.php";
 include_once "knowledge.class.php";
 include_once "auth.class.php";
+include_once "availability.class.php";
 
 
 ?>
